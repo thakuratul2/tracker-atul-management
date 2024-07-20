66fd895a39655321cca59d2fc6eaae3915671d02
thakuratul2 <pratapsinghatul111@gmail.com>
2024-07-20 11:35:58 +0530

    Modify in password column

--- clients/client_edit.php
+++ clients/client_edit.php
@@ -74,7 +74,7 @@ user_not_login();
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" value="<?php echo $password ?>"  name="password">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"  name="password">
                         </div>
                         
                         <div class="form-group">
