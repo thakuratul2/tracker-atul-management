c6e341341b672fed13b4b0e888f40013128310d5
thakuratul2 <pratapsinghatul111@gmail.com>
2024-08-31 22:54:03 +0530

    Updated: Project name and project type

--- method/task_method.php
+++ method/task_method.php
@@ -2,22 +2,19 @@
 include_once ('../connection/db.php');
 include_once ('../connection/common/session.php');
 
-
 $title = $_POST['title'];
 $task_start = $_POST['task_start'];
 $start_time = $_POST['start_time'];
-$task_end = $_POST['task_end'];
-$end_time = $_POST['end_time'];
-$description = $_POST['description'];
+$project_type = $_POST['project_type'];  
+$task_type = $_POST['task_type'];
 $add_user = $_SESSION['user_name'];
 $role_id = $_SESSION['user_role'];
 
-$sql = "INSERT INTO tasks (title, task_start, start_time, task_end, end_time, description, add_user, role_id) VALUES ('$title', '$task_start', '$start_time', '$task_end', '$end_time', '$description', '$add_user', '$role_id')";
+$sql = "INSERT INTO tasks (title, task_start, start_time, project_type, task_type, add_user, role_id) VALUES ('$title', '$task_start', '$start_time', '$project_type', '$task_type', '$add_user', '$role_id')";
 $row = mysqli_query($conn, $sql);
 
 if($row){
     echo "Success";
 }else{
-
-    echo "Task Not Added";
+    echo "Task Not Added: " . mysqli_error($conn);  // Add error message for debugging
 }
